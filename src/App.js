0cd885487e0bc450fd1eeefa4b969f7d0a007c25
Cüneyt Kaya <kayacuneyd@gmail.com>
2023-06-17 12:54:32 +0200

    https request of api url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 
 const API_URL =
-  "http://www.omdbapi.com?apikey=" + process.env.REACT_APP_API_URL;
+  "https://www.omdbapi.com?apikey=" + process.env.REACT_APP_API_URL;
 
 function App() {
   const [movies, setMovies] = useState([]);
